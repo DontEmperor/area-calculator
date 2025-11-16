1691093598f103b585c2d734bfd6ce4ea6a833b0
DontEmperor <noname1@NoName.localdomain>
2025-11-16 15:49:53 +0400

    Добавлен скрипт для расчёта площади

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py — калькулятор площади прямоугольника
+author = "Шестериков Андрей"
+width = 10
+height = 20
+
+area = width * height
+
+print(f"Расчет площади прямоугольника от {author}")
+print(f"Ширина: {width}")
+print(f"Высота: {height}")
+print("-" * 20)
+print(f"Площадь: {area}")
